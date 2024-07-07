6608763c6e3d62cc353e5c674ecc10374283a04a
camilo <lafirmafuerte@gmail.com>
2024-07-07 16:41:39 -0300

    fix: some bug3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edithhh <code>src/App.js</code> and savfffffffffffe to reload.
+          Edithhh <code>src/App.js</code> and sav to reload.
         </p>
         <a
           className="App-link"
